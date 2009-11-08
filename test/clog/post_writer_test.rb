033011f787c2f65a354248109fceb7113828187f
Ryan Kinderman <ryan@kinderman.net>
2009-11-08 13:26:45 -0600

    removed PostWriter

--- test/clog/post_writer_test.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '/../test_helper'))
-require 'stringio'
-
-module Clog
-  class PostWriterTest < Test::Unit::TestCase
-    def test_nothing; end
-    
-  end
-end
